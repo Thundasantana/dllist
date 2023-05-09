330f911f8228bd909810772c129ae61626387686
Ednilson Santana <ednilson.sdas@outlook.com>
2023-05-09 19:50:33 -0300

    Project created

---
+++ src/test/java/com/devlyon/dllist/DllistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlyon.dllist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DllistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
