13d8f31b13fe77021f8b6ab852d9c8515138d9e0
Ednilson Santana <ednilson.sdas@outlook.com>
2023-05-09 21:42:54 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devlyon/dllist/controllers/GameController.java
@@ -0,0 +1,26 @@
+package com.devlyon.dllist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devlyon.dllist.dto.GameMinDTO;
+import com.devlyon.dllist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll() {
+		List<GameMinDTO> result = gameService.findAll();
+		return result;
+	}
+
+}
